3cf6fbf055806cbc35e849f67b9a780040597f03
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-10-25 15:27:03 +0200

    create socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include "Log.h"
+#include "Socket.h"
 
 int main() {
     Log::open();
     Log::info("Launching BomberStudent...");
-    Log::info("BomberStudent launched");
+    try {
+        Socket socket=Socket(SocketAddress("localhost",42069,Protocol::IPV6));
+        Log::info("BomberStudent launched");
+    } catch (std::exception const&) {
+        Log::error("BomberStudent launching failed");
+    }
+    Log::info("BomberStudent stopped");
     Log::close();
 }
