05b8408ba9a248de2a657d1b1202d8a7b378b4b7
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-10-25 22:26:45 +0200

    use IPv6

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main() {
     Log::open();
     Log::info("Launching BomberStudent...");
     try {
-        SocketAddress addr1 = SocketAddress("127.0.0.1",42069,Protocol::IPV4);
-        SocketAddress addr2 = SocketAddress("127.0.0.1",42469,Protocol::IPV4);
+        SocketAddress addr1 = SocketAddress("127.0.0.1",42069,Protocol::IPV6);
+        SocketAddress addr2 = SocketAddress("127.0.0.1",42469,Protocol::IPV6);
         Socket socket1=Socket(addr1);
         Socket socket2=Socket(addr2);
         socket1.send("hello world !\n", addr2);
