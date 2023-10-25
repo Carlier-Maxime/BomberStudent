1f377c8fdba14457c888fd19ccc2366c4f08c173
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-10-25 16:31:59 +0200

    simple example for send and receive

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,12 @@ int main() {
     Log::open();
     Log::info("Launching BomberStudent...");
     try {
-        Socket socket=Socket(SocketAddress("localhost",42069,Protocol::IPV6));
+        SocketAddress addr1 = SocketAddress("127.0.0.1",42069,Protocol::IPV6);
+        SocketAddress addr2 = SocketAddress("127.0.0.1",42469,Protocol::IPV6);
+        Socket socket1=Socket(addr1);
+        Socket socket2=Socket(addr2);
+        socket1.send("hello world !\n", addr2);
+        std::cout << socket2.receive(addr1);
         Log::info("BomberStudent launched");
     } catch (std::exception const&) {
         Log::error("BomberStudent launching failed");
