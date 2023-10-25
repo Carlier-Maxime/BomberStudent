8b5b3b2ba51ab189924e491ce1a157124d6e24d0
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-10-25 16:30:40 +0200

    add send and receive

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -4,13 +4,19 @@
 #include <unistd.h>
 #include "Log.h"
 
-Socket::Socket(SocketAddress addr) : addr(addr) {
-    struct sockaddr_in addr_in{
-            AF_INET,
-            htons(addr.getPort()),
-            {inet_addr(addr.getIp().c_str())},
-            {0,0,0,0,0,0,0,0}
+#define BUFFER_SIZE 1024
+
+struct sockaddr_in getSockAddrIn(const SocketAddress& addr) {
+    return {
+        AF_INET,
+        htons(addr.getPort()),
+        {inet_addr(addr.getIp().c_str())},
+        {0,0,0,0,0,0,0,0}
     };
+}
+
+Socket::Socket(SocketAddress addr) : addr(addr) {
+    struct sockaddr_in addr_in = getSockAddrIn(addr);
     if ((socket_fd = socket(addr_in.sin_family, SOCK_DGRAM, 0)) < 0) { 
         Log::system_error("create socket failed");
         throw std::exception();
@@ -24,4 +30,23 @@ Socket::Socket(SocketAddress addr) : addr(addr) {
 
 Socket::~Socket() {
     close(socket_fd);
+}
+
+void Socket::send(std::string msg, const SocketAddress& dest_addr) {
+    struct sockaddr_in addr_in = getSockAddrIn(dest_addr);
+    if (sendto(socket_fd, msg.c_str(), msg.length(), 0, (struct sockaddr*)&addr_in, sizeof(addr_in)) < 0) {
+        Log::system_error("Socket can't send");
+        throw std::exception();
+    }
+}
+
+std::string Socket::receive(const SocketAddress& src_addr) {
+    struct sockaddr_in addr_in = getSockAddrIn(src_addr);
+    socklen_t src_socket_size = sizeof(addr_in);
+    char buffer[BUFFER_SIZE];
+    if (recvfrom(socket_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&addr_in, &src_socket_size) < 0) {
+        Log::system_error("Couldn't receive");
+        throw std::exception();
+    }
+    return std::string(buffer);
 }
--- src/Socket.h
+++ src/Socket.h
@@ -10,6 +10,8 @@ private:
     int socket_fd;
 public:
     Socket(SocketAddress addr);
+    void send(std::string msg, const SocketAddress& dest_addr);
+    std::string receive(const SocketAddress& src_addr);
     ~Socket();
 };
 
