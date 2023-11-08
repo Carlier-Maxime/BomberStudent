2485834931e077a2074cc881ccd54eec44b0d3f5
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-11-08 22:33:08 +0100

    test receive

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,12 @@ int main() {
     try {
         SocketAddress addr1 = SocketAddress("127.0.0.1",42069,Protocol::IPV6);
         SocketAddress addr2 = SocketAddress("127.0.0.1",42469,Protocol::IPV6);
+        SocketAddress addr3 = SocketAddress("127.0.0.1",42569,Protocol::IPV6);
         Socket socket1=Socket(addr1);
         Socket socket2=Socket(addr2);
         socket1.send("hello world !\n", addr2);
-        std::cout << socket2.receive(addr1);
+        std::cout << socket2.receive(&addr3);
+        std::cout << addr3 << std::endl;
         Log::info("BomberStudent launched");
     } catch (std::exception const&) {
         Log::error("BomberStudent launching failed");
