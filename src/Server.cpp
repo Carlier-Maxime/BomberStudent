f880dee16d29b56a7c38796051809b8503dd78aa
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-11-09 23:02:43 +0100

    add Log information

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,11 +1,14 @@
 #include "Server.h"
+#include "Log.h"
 
-Server::Server() : socket(SocketAddress("::1",42069,Protocol::IPV6)) {}
+Server::Server() : socket(SocketAddress("::1",42069,Protocol::IPV6), true) {}
 
 Server::~Server() = default;
 
 void Server::run() {
-    SocketAddress address = SocketAddress("::1", 0, Protocol::IPV6);
+    SocketAddress address = SocketAddress("::", 0, Protocol::IPV6);
+    Log::info("waiting for client...");
     while (socket.receive(&address)!="looking for bomberstudent servers");
     socket.send("hello i’m a bomberstudent server", address);
+    Log::info("client found !");
 }
