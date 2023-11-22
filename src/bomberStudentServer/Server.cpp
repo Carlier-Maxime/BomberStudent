ca30fa4b79c5922671cd2867d7e6ffb76b57a7b5
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-11-22 15:44:00 +0100

    use program handlerUDP for loopUDP

--- src/bomberStudentServer/Server.cpp
+++ src/bomberStudentServer/Server.cpp
@@ -1,11 +1,9 @@
 #include "Server.h"
 #include "../utils/Log.h"
 #include "../utils/Config.h"
-#include "../utils/ConstantMessages.h"
-#include "../utils/Utils.h"
 #include "../utils/BomberStudentExceptions.h"
 #include <unistd.h>
-#include <csignal>
+#include <sys/wait.h>
 
 Server::Server() : address(SocketAddress("::1",Config::getServerPort())), socketTCP(address.getProtocol()) {
     socketTCP.bind(address);
@@ -14,25 +12,17 @@ Server::Server() : address(SocketAddress("::1",Config::getServerPort())), socket
 
 Server::~Server() = default;
 
-[[noreturn]] void Server::loopUDP() {
-    Utils::processName= "Server:UDP";
-    auto socketUDP = SocketUDP(address.getProtocol(), true);
-    socketUDP.bind(address);
-    SocketAddress client = SocketAddress("::", 0);
-    for (;;) {
-        Log::info("waiting client...");
-        while (socketUDP.receive(&client)!=ConstantMessages::lookingServers);
-        Log::info("client found !");
-        socketUDP.send(ConstantMessages::serverHello, client);
-    }
-}
-
 void Server::run() {
     int pid;
-    if ((pid=fork())==-1) throw SystemException("fork failed");
-    else if (pid==0) loopUDP();
+    if ((pid=fork())==-1) throw SystemException("Launching handler UDP failed (fork)");
+    else if (pid==0) {
+        char* args[] = {(char*) "./handlerUDP", nullptr};
+        execv(args[0], args);
+        throw SystemException("Launching handler UDP failed (exec)");
+    }
     SocketAddress client = SocketAddress("::", 0);
     auto socket = socketTCP.accept();
     Log::info("client connected !");
     kill(pid,2);
+    waitpid(pid, nullptr, 0);
 }
---
+++ src/handlerUDP/handlerUDP.cpp
@@ -0,0 +1,37 @@
+#include <csignal>
+
+#include "../socket/SocketAddress.h"
+#include "../socket/SocketUDP.h"
+#include "../utils/Config.h"
+#include "../utils/ConstantMessages.h"
+#include "../utils/Log.h"
+#include "../utils/Utils.h"
+#include "../utils/BomberStudentExceptions.h"
+
+int main() {
+    Utils::processName= "Server:UDP";
+    Log::open();
+    SocketAddress address = SocketAddress("::1",Config::getServerPort());
+    auto socketUDP = SocketUDP(address.getProtocol(), true);
+    socketUDP.bind(address);
+    SocketAddress client = SocketAddress("::", 0);
+    const struct sigaction act = {
+            [](int){},
+            {},
+            0,
+            {}
+    };
+    sigaction(SIGINT, &act, nullptr);
+    for (;;) {
+        Log::info("waiting client...");
+        try {
+            while (socketUDP.receive(&client)!=ConstantMessages::lookingServers);
+        } catch (SocketException& e) {
+            break;
+        }
+        Log::info("client found !");
+        socketUDP.send(ConstantMessages::serverHello, client);
+    }
+    Log::info("shutdown handler UDP");
+    Log::close();
+}
