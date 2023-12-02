271ccaf20a3f7eef3fda9f9d6411b957d28a570d
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2023-12-02 19:54:10 +0100

    generate random number

--- src/utils/Utils.h
+++ src/utils/Utils.h
@@ -3,12 +3,16 @@
 
 
 #include <filesystem>
+#include <random>
 
 class Utils {
+private:
+    static std::mt19937 random_generator;
 public:
     static std::filesystem::path getProgramPath();
     static std::string processName;
     static thread_local std::string threadName;
+    static u_long getRandomNumber(u_long min, u_long max);
 };
 
 
