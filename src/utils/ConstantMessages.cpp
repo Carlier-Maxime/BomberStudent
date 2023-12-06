3573731dc7b4841123a1bf0f71efef2ab384ffe0
Carlier Maxime <maxime_carlier@ens.univ-artois.fr>
2023-12-06 14:39:09 +0100

    using CM for ConstantMessages

--- src/utils/ConstantMessages.cpp
+++ src/utils/ConstantMessages.cpp
@@ -1,15 +1,17 @@
 #include "ConstantMessages.h"
 #include "../json/JSONMessage.h"
 
-const std::string ConstantMessages::lookingServers = "looking for bomberstudent servers";
-const std::string ConstantMessages::serverHello = "hello i’m a bomberstudent server";
-const std::string ConstantMessages::getMapList = "GET maps/list";
-const std::string ConstantMessages::badRequest = JSONMessage::errorMessage(400, "Bad request");
-const std::string ConstantMessages::getGameList = "GET game/list";
-const std::string ConstantMessages::postGameCreate = "POST game/create";
-const std::string ConstantMessages::failedCreateGame = JSONMessage::actionMessage("game/create", 501, "cannot create game", "");
-const std::string ConstantMessages::postGameJoin = "POST game/join";
-const std::string ConstantMessages::failedJoinGame = JSONMessage::actionMessage("game/join", 501, "cannot join the game", "");
-const std::string ConstantMessages::postGameStart = "POST game/start";
-const std::string ConstantMessages::postGameReady = "POST game/ready\n{\"time\":3}";
-const std::string ConstantMessages::postGameGo = "POST game/go";
+using CM = ConstantMessages;
+
+const std::string CM::lookingServers = "looking for bomberstudent servers";
+const std::string CM::serverHello = "hello i’m a bomberstudent server";
+const std::string CM::getMapList = "GET maps/list";
+const std::string CM::badRequest = JSONMessage::errorMessage(400, "Bad request");
+const std::string CM::getGameList = "GET game/list";
+const std::string CM::postGameCreate = "POST game/create";
+const std::string CM::failedCreateGame = JSONMessage::actionMessage("game/create", 501, "cannot create game", "");
+const std::string CM::postGameJoin = "POST game/join";
+const std::string CM::failedJoinGame = JSONMessage::actionMessage("game/join", 501, "cannot join the game", "");
+const std::string CM::postGameStart = "POST game/start";
+const std::string CM::postGameReady = "POST game/ready\n{\"time\":3}";
+const std::string CM::postGameGo = "POST game/go";
