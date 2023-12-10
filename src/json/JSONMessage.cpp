1716bd32a316b504b719c5947b8b70f50f4ce366
M_EtOuais <28m06c02@gmail.com>
2023-12-10 13:07:53 +0100

    remove no used msg and update alertBombPosed

--- src/json/JSONMessage.cpp
+++ src/json/JSONMessage.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <sstream>
 #include "JSONMessage.h"
+#include "../utils/ConstantMessages.h"
 
+using CM = ConstantMessages;
 
 std::string JSONMessage::errorMessage(int statut, const std::string& message) {
 	std::ostringstream errorMessage;
@@ -15,35 +17,15 @@ std::string JSONMessage::actionMessage(const std::string& action, int statut, co
     return json.str();
 }
 
-std::string JSONMessage::playerPositionUpdateMessage(Player* player, const std::string& dir) {
-	std::string name = player->getName();
-	std::ostringstream message;
-	message << "POST player/position/update\n{\"player\":\""<< name << R"(","dir":")"<< dir <<"\"}";
-	return message.str();
-}
-
-std::string JSONMessage::updatePoseBombPlayerMessage(Player* playerState, int posX, int posY) {
-	std::string action = "attack/bomb";
-	int statut = 201;
-	std::ostringstream msg;
-	msg << "bomb is armed at pos " << posY << "," << posX;
-	std::string playerStateJSONString = playerState->toJSON();
+std::string JSONMessage::alertBombPosedMessage(const std::string &type, u_char posX, u_char posY) {
 	std::ostringstream json;
-	json << R"({"action":")"<< action <<R"(","statut":")" << statut << R"(","message":")"<< msg.str() <<R"(","player": )"<< playerStateJSONString <<"}";
-
-	return json.str();
-
-}
-std::string JSONMessage::alertBombPosedMessage(int posX, int posY, const std::string& type) {
-	std::ostringstream json;
-	json << "POST attack/newbomb\n{\"pos\":\""<< posY << ","<< posX << R"(","type":")"<< type <<"\"}";
+	json << CM::postAttackNewBomb << "\n{\"pos\":\""<< posY << ","<< posX << R"(","type":")"<< type <<"\"}";
 	return json.str();
 }
 
 std::string JSONMessage::alertBombExplodedMessage(int posX, int posY, const std::string& type, int impactDist, const std::string& map) {
 	std::ostringstream json;
 	json <<  "POST attack/explose\n{\"pos\":\""<< posY <<","<< posX <<R"(","type":")"<< type <<R"(","impactDist":)"<< impactDist << R"(,"map":")"<< map <<"\"}";
-
 	return json.str();
 }
 
