8cd892d81a288b526bc8821bacb16743264d925d
M_EtOuais <28m06c02@gmail.com>
2023-12-16 22:04:58 +0100

    remove json msg present in other class

--- src/json/JSONMessage.cpp
+++ src/json/JSONMessage.cpp
@@ -17,19 +17,6 @@ std::string JSONMessage::actionMessage(const std::string& action, int statut, co
     return json.str();
 }
 
-std::string JSONMessage::alertBombExplodedMessage(int posX, int posY, const std::string& type, int impactDist, const std::string& map) {
-	std::ostringstream json;
-	json <<  "POST attack/explose\n{\"pos\":\""<< posY <<","<< posX <<R"(","type":")"<< type <<R"(","impactDist":)"<< impactDist << R"(,"map":")"<< map <<"\"}";
-	return json.str();
-}
-
-std::string JSONMessage::alertAttackAffectMessage(Player* playerState) {
-	std::string playerStateJSONString = playerState->toJSON();
-	
-	std::ostringstream json;
-	json << "POST attack/affect\n" << playerStateJSONString;
-	return json.str();
-}
 std::string JSONMessage::bonusUpdateMessage(Player* playerState) {
 	std::string action = "object/new";
 	std::string msg="ok";
